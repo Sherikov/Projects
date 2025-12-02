0ab2ef3e8eb31ebc9c41db0acd090dfabf74804c
Sherikov Nafasbek <sherikov966@gmail.com>
2025-12-02 15:28:46 +0500

    Fixed Robot-School page

--- WebSite/Практика-сайты/Robot-school/src/js/script.js
+++ WebSite/Практика-сайты/Robot-school/src/js/script.js
@@ -1,9 +1,12 @@
-const fourSectionScrolling = document.querySelector('.four-section-scrolling');
-fourSectionScrolling.addEventListener('wheel', function(ev) {
-  let fourSectionScrollingCards = this.querySelectorAll('.four-section-scrolling__cards');
-  if (ev.wheelDelta > 0) {
-    this.prepend(fourSectionScrollingCards[fourSectionScrollingCards.length - 1]);
-  } else {
-    this.append(fourSectionScrollingCards[0]);
-  }
+const track = document.querySelector('.carousel-track');
+
+// 1. Берем все карточки, которые есть сейчас
+const cards = Array.from(track.children);
+
+// 2. Клонируем каждую карточку и добавляем в конец ленты
+// Теперь у нас будет набор: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+cards.forEach(card => {
+    const clone = card.cloneNode(true);
+    track.appendChild(clone);
 });
+
